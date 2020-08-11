63a4225135891d1c31da9fa2319307817b322302
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-08-11 18:01:13 -0500

    Basic index and 404 pages

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const app = express()
 const PORT = 3001
 
 const apiRoutes = require('./routes/api')
+const mainRoutes = require('./routes/index')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/api', apiRoutes)
+app.use(mainRoutes)
 
 app.listen(PORT)
 // eslint-disable-next-line no-console
