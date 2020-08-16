0d40de1d65aece313a4ca8c7015e267fc2ab8b2c
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-08-16 17:39:32 -0500

    Fixing heartbeat route

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -4,6 +4,10 @@ const wrhaRoutes = require('./wrha')
 
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  res.json({ message: 'Hello from API' })
+})
+
 router.use('/wrha', wrhaRoutes)
 
 router.use((req, res) => {
