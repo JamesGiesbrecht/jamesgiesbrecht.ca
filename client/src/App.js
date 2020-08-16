3313014a8f61cef1f9347da571f0832b2cbc919c
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-08-16 13:32:43 -0500

    Testing proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
+const App = () => {
+  const [apiRes, setApiRes] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    axios.get('/api')
+      .then((response) => {
+        console.log(response)
+        setApiRes(response.data.message)
+      })
+      .catch((error) => {
+        console.log(error)
+        setApiRes('An error occured')
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code> src/App.js </code>
-          and save to reload.
-        </p>
+        <p>{isLoading ? 'Loading...' : apiRes}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
