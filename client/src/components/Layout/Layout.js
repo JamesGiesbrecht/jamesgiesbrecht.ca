b5e525e3a5834e3192a9b4e3be9ddc4862b383e0
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-08-22 14:03:31 -0500

    Nav bar

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Header from './Header'
+import NavBar from './NavBar'
 import Footer from './Footer'
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Header />
+      <NavBar />
       {children}
       <Footer />
     </>
---
+++ client/src/components/Layout/NavBar.js
@@ -0,0 +1,136 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import { Typography, IconButton, Popper, MenuItem, Tabs, Tab, Paper, Grow, MenuList, ClickAwayListener, useMediaQuery } from '@material-ui/core'
+import { Home, Code, Mail, Menu as MenuIcon } from '@material-ui/icons'
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  navBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingLeft: '15px',
+  },
+  title: {
+    margin: 'auto',
+    // color: theme.palette.secondary.main,
+  },
+  iconPadding: {
+    paddingLeft: '0',
+  },
+  desktopNav: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'flex',
+    },
+  },
+  mobileNav: {
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+}))
+
+const NavBar = () => {
+  const classes = useStyles()
+  const [isOpen, setIsOpen] = useState(null)
+  const [activeNav, setActiveNav] = useState('Home')
+  const anchorRef = useRef(null)
+
+  const handleToggle = () => {
+    setIsOpen((prevOpen) => !prevOpen)
+  }
+
+  const handleClose = (e) => {
+    if (anchorRef.current && anchorRef.current.contains(e.target)) {
+      return
+    }
+    setIsOpen(false)
+  }
+
+  const handleTabChange = (e, newValue) => {
+    setActiveNav(newValue)
+}
+
+  const navItems = [
+    { name: 'Home', icon: <Home /> },
+    { name: 'Projects', icon: <Code /> },
+    { name: 'Contact', icon: <Mail /> },
+  ]
+
+  const desktopNav = navItems.map((nav) => (
+    <Tab
+      key={nav.name}
+      label={nav.name}
+      value={nav.name}
+    />
+  ))
+
+  const mobileNav = navItems.map((nav) => (
+    <MenuItem
+      key={nav.name}
+      onClick={(e) => {
+        setActiveNav(nav.name)
+        handleClose(e)
+      }}
+    >
+      <IconButton classes={{ root: classes.iconPadding }} color="inherit">
+        {nav.icon}
+      </IconButton>
+      <p>{nav.name}</p>
+    </MenuItem>
+  ))
+
+  const mobileMenu = (
+    <Popper
+      anchorEl={anchorRef.current}
+      open={isOpen}
+      transition
+      disablePortal
+    >
+      {({ TransitionProps, placement }) => (
+        <Grow
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          {...TransitionProps}
+          style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+          className={classes.mobileNav}
+        >
+          <Paper square>
+            <ClickAwayListener onClickAway={handleClose}>
+              <MenuList autoFocusItem={isOpen}>
+                {mobileNav}
+              </MenuList>
+            </ClickAwayListener>
+          </Paper>
+        </Grow>
+      )}
+    </Popper>
+  )
+
+  return (
+    <Paper className={classes.navBar} square>
+      <Typography variant="h6" className={classes.title}>
+        JG
+      </Typography>
+      <div className={classes.grow} />
+      <div className={classes.desktopNav}>
+        <Tabs value={activeNav} onChange={handleTabChange}>
+          {desktopNav}
+        </Tabs>
+      </div>
+      <div className={classes.mobileNav}>
+        <IconButton
+          onClick={handleToggle}
+          color="inherit"
+          ref={anchorRef}
+        >
+          <MenuIcon />
+        </IconButton>
+      </div>
+      {mobileMenu}
+    </Paper>
+  )
+}
+
+export default NavBar
