231f7644141a921fd99d21e20c09dfe22c5f7505
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-08-22 18:19:23 -0500

    Projects stub

---
+++ client/src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+
+const Projects = () => {
+  return (
+    <div>
+      <Typography variant="h3">Projects</Typography>
+      <Typography>Coming soon...</Typography>
+    </div>
+  )
+}
+
+export default Projects
