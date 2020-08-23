adeeb465c55a22c3b3d597400a8a76a4105a9198
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-08-23 17:34:21 -0500

    Adding tech and project data

--- client/src/assets/projects.js
+++ client/src/assets/projects.js
@@ -1,6 +1,12 @@
 import nodeJs from './img/logos/node-js.svg'
 import react from './img/logos/react.svg'
 import nodeHex from './img/logos/node-hex.png'
+import express from './img/logos/express.png'
+import php from './img/logos/php.png'
+import materialUI from './img/logos/material-ui.png'
+import javascript from './img/logos/javascript.png'
+import css from './img/logos/css.png'
+import bootstrap from './img/logos/bootstrap.svg'
 
 import onesnap from './img/projects/onesnap.png'
 
@@ -19,29 +25,54 @@ const tech = {
   },
   materialUI: {
     name: 'Material UI',
-    color: '#2384C7',
-    fontColor: '#FFFFFF',
+    logo: materialUI,
+    color: '#2A2C2E',
+    fontColor: '#00B3FA',
+  },
+  express: {
+    name: 'Express',
+    logo: express,
+    color: '#FFFFFF',
+    fontColor: '#000000',
   },
   php: {
     name: 'PHP',
-    color: '#222222',
-    fontColor: '#FFFFFF',
+    logo: php,
+    color: '#717AAA',
+    fontColor: '#000000',
   },
   javascript: {
     name: 'Javascript',
-    color: '#222222',
-    fontColor: '#FFFFFF',
+    logo: javascript,
+    color: '#F0DB4F',
+    fontColor: '#000000',
   },
   css: {
     name: 'CSS',
-    color: '#222222',
+    logo: css,
+    color: '#2965F1',
     fontColor: '#FFFFFF',
   },
+  bootstrap: {
+    name: 'Bootstrap',
+    logo: bootstrap,
+    color: '#563D7C',
+    fontColor: '#FFFFFF',
+  }
 }
 
 export default [
   {
+    name: 'JamesGiesbrecht.ca',
+    summary: 'The website you\'re on right now!',
+    description: 'This website',
+    link: '#',
+    github: 'https://github.com/JamesGiesbrecht/james-giesbrecht',
+    stack: [tech.react, tech.materialUI, tech.node, tech.express],
+  },
+  {
     name: 'Critter Companion',
+    summary: 'A basic progress tracker for Animal Crossing New Horizons.',
     description: 'A way for users to track their collections of critters in Animal Crossing New Horizons.',
     link: 'https://crittercompanion.app',
     github: 'https://github.com/JamesGiesbrecht/acnh-critter-companion',
@@ -49,9 +80,10 @@ export default [
   },
   {
     name: 'Onesnap',
+    summary: 'Instagram Clone for a college final project.',
     description: 'An Instagram clone made for a final project in a web development class at Red River College.',
     image: onesnap,
     github: 'https://github.com/JamesGiesbrecht/Onesnap',
-    stack: [tech.php, tech.javascript, tech.css],
+    stack: [tech.php, tech.javascript, tech.css, tech.bootstrap],
   },
 ]
