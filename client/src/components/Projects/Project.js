45f5333fcd201af0522fe7a68b5d43bb3ac7de3e
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-08-23 14:30:57 -0500

    Project card images, links

--- client/src/components/Projects/Project.js
+++ client/src/components/Projects/Project.js
@@ -1,23 +1,74 @@
 import React from 'react'
-import { Card, CardHeader, CardMedia, CardContent, CardActions, ButtonGroup, Button } from '@material-ui/core'
+import { Card, CardHeader, CardMedia, CardContent, CardActions, ButtonGroup, Button, makeStyles, Typography } from '@material-ui/core'
+import { Link as LinkIcon, Code as CodeIcon } from '@material-ui/icons'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+    margin: theme.spacing(3, 'auto'),
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+    backgroundColor: 'white',
+  },
+  link: {
+    marginRight: theme.spacing(1),
+  },
+}))
 
 const Project = ({ project }) => {
+  const classes = useStyles()
+
+  const getButton = (link, type) => {
+    let icon
+    switch (type) {
+      case 'Code':
+        icon = <CodeIcon className={classes.link} />
+        break
+      case 'Website':
+      default:
+        icon = <LinkIcon className={classes.link} />
+    }
+
+    if (link) {
+      return (
+        <Button
+          href={link}
+        >
+          {icon}
+          {type}
+        </Button>
+      )
+    }
+    return null
+  }
+
+  const buttons = (project.link || project.github)
+    ? (
+      <ButtonGroup>
+        {getButton(project.link, 'Website')}
+        {getButton(project.github, 'Code')}
+      </ButtonGroup>
+    )
+    : null
+
   return (
-    <Card>
-      <CardHeader>
-        {project.name}
-      </CardHeader>
+    <Card className={classes.root}>
+      <CardHeader
+        title={project.name}
+        subheader={project.summary}
+      />
       <CardMedia
+        className={classes.media}
         image={project.image}
+        title={project.name}
       />
       <CardContent>
-        {project.description}
+        <Typography variant="body2" color="textSecondary" component="p">{project.description}</Typography>
       </CardContent>
       <CardActions>
-        <ButtonGroup>
-          <Button>View Website</Button>
-          <Button>View Code</Button>
-        </ButtonGroup>
+        {buttons}
       </CardActions>
     </Card>
   )
