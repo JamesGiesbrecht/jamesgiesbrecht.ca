e660ec9eec5c1edc7ab05c8bd8a6f0facb1ec226
JamesGiesbrecht <giesbrechtjames@gmail.com>
2020-08-29 15:48:21 -0500

    Setting port for dev

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const { public } = require('./util/path')
 
 const app = express()
-const PORT = 3001
+const PORT = process.env.NODE_ENV || 3001
 
 const mainRoutes = require('./routes/index')
 const apiRoutes = require('./routes/api/index')
