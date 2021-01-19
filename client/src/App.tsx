4114c1c22fe3b0f63edbcbe3b9f300b8722aba03
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-01-18 21:21:53 -0600

    Updating index and app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,10 @@ import useColorScheme from 'hooks/useColorScheme'
 import Projects from 'components/Projects'
 // import ScreenSize from 'components/ScreenSize'
 
+/*
+TODO: Add bio, contact section, multiple links for code button
+*/
+
 const App = () => {
   /* THEMING AND STYLES START */
   const [colorScheme, toggleColorScheme] = useColorScheme()
