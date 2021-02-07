ac1cb26b279d99df2a2b9fbd3188aa8c648a2e92
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-02-06 20:58:05 -0600

    Temp fix for lint error

--- client/src/components/Posts.tsx
+++ client/src/components/Posts.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Container, Typography } from '@material-ui/core'
+import { AuthContext } from 'context/Auth'
 
 const Posts: React.FC = () => {
+  const { user } = useContext(AuthContext)
+
   return (
     <Container>
       <Typography variant="h3">Posts</Typography>
