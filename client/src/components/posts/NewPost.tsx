bef0ee0f48a8266da52602d7a7c8b3ad9084a9da
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-02-10 20:52:26 -0600

    Adding the new post form as a modal

--- client/src/components/posts/NewPost.tsx
+++ client/src/components/posts/NewPost.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { AxiosResponse } from 'axios'
-import { Button, Card, TextField, makeStyles, Typography, FormControlLabel, Switch, CircularProgress, Fab } from '@material-ui/core'
-import { Add } from '@material-ui/icons'
+import { Button, Card, TextField, makeStyles, Typography, FormControlLabel, Switch, CircularProgress, Fab, Modal, Box, IconButton, useMediaQuery, Theme } from '@material-ui/core'
+import { Add, Close } from '@material-ui/icons'
 import useApi from 'hooks/useApi'
 
 const useStyles = makeStyles((theme) => ({
@@ -9,10 +9,24 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2),
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(2),
-    maxWidth: 500,
+    width: 500,
+    maxWidth: '90%',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
   },
   button: {
-    marginRight: theme.spacing(1),
+    [theme.breakpoints.down('xs')]: {
+      position: 'fixed',
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+  },
+  buttonIcon: {
+    [theme.breakpoints.up('sm')]: {
+      marginRight: theme.spacing(1),
+    },
   },
   form: {
     display: 'flex',
@@ -40,11 +54,17 @@ interface Props {
 
 const NewPost: React.FC<Props> = ({ setPosts }) => {
   const classes = useStyles()
+  const [modalOpen, setModalOpen] = useState<boolean>(false)
   const [title, setTitle] = useState<string>('')
   const [content, setContent] = useState<string>('')
   const [isPublic, setIsPublic] = useState<boolean>(false)
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
   const api = useApi()
+  const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('xs'))
+
+  const handleModalOpen = () => setModalOpen(true)
+
+  const handleModalClose = () => setModalOpen(false)
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -58,19 +78,33 @@ const NewPost: React.FC<Props> = ({ setPosts }) => {
         setPosts((prev: any) => [result.data, ...prev])
       })
       .catch((error: any) => console.log(error))
-      .finally(() => setIsSubmitting(false))
+      .finally(() => {
+        setIsSubmitting(false)
+        handleModalClose()
+      })
   }
 
   const newPostButton = (
-    <Fab color="primary" variant="extended" size="medium">
-      <Add className={classes.button} />
-      New Post
+    <Fab
+      className={classes.button}
+      color="primary"
+      variant={isMobile ? 'round' : 'extended'}
+      size={isMobile ? 'large' : 'medium'}
+      onClick={handleModalOpen}
+    >
+      <Add className={classes.buttonIcon} />
+      {!isMobile && 'New Post'}
     </Fab>
   )
 
   const newPostForm = (
     <Card className={classes.card}>
-      <Typography variant="h5">Make a New Post</Typography>
+      <Box display="flex" flexDirection="row" justifyContent="space-between" alignItems="center">
+        <Typography variant="h5">Make a New Post</Typography>
+        <IconButton onClick={handleModalClose}>
+          <Close />
+        </IconButton>
+      </Box>
       <form className={classes.form} onSubmit={handleSubmit}>
         <TextField
           variant="filled"
@@ -117,7 +151,17 @@ const NewPost: React.FC<Props> = ({ setPosts }) => {
     </Card>
   )
 
-  return newPostButton
+  return (
+    <>
+      {newPostButton}
+      <Modal
+        open={modalOpen}
+        onClose={handleModalClose}
+      >
+        {newPostForm}
+      </Modal>
+    </>
+  )
 }
 
 export default NewPost
