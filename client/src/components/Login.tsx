b1a3850ba4f06dd1d44d9d69473a22a51230df8b
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-02-13 10:30:47 -0600

    Hiding login buttons while logging in

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,10 +1,11 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AxiosResponse } from 'axios'
 import FacebookLogin from 'react-facebook-auth'
 import GoogleLogin, { GoogleLoginResponse, GoogleLoginResponseOffline } from 'react-google-login'
 import { Button, makeStyles, Typography, Container } from '@material-ui/core'
 import { Facebook, Apple, GitHub, Twitter } from '@material-ui/icons'
 import GoogleIcon from 'components/Icons/GoogleIcon'
+import WaitFor from 'components/utility/WaitFor'
 import { AuthContext } from 'context/Auth'
 import { useHistory } from 'react-router-dom'
 import useApi from 'hooks/useApi'
@@ -30,6 +31,7 @@ const useStyles = makeStyles((theme) => ({
 const Login: React.FC = () => {
   const classes = useStyles()
   const { user, setUser } = useContext(AuthContext)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
   const api = useApi()
   const history = useHistory()
 
@@ -49,7 +51,7 @@ const Login: React.FC = () => {
     console.log(response)
     if (!isGoogleLoginResponse(response)) return
     const token = response.tokenId
-
+    setIsLoading(true)
     api.post('/google-login', {
       idToken: response.tokenId,
     })
@@ -62,6 +64,7 @@ const Login: React.FC = () => {
         console.log(res)
       })
       .catch((err: any) => console.log(err))
+      .finally(() => setIsLoading(false))
   }
 
   const responseErrorGoogle = (response: any): void => {
@@ -71,64 +74,66 @@ const Login: React.FC = () => {
   return (
     <Container className={classes.loginButtons}>
       <Typography className={classes.title} variant="h3">Sign In</Typography>
-      <GoogleLogin
-        clientId={process.env.REACT_APP_GOOGLE_APP_ID as string}
-        onSuccess={responseSuccessGoogle}
-        onFailure={responseErrorGoogle}
-        cookiePolicy="single_host_origin"
-        render={(renderProps) => (
-          <Button
-            onClick={renderProps.onClick}
-            disabled={renderProps.disabled}
-            variant="outlined"
-            startIcon={<GoogleIcon />}
-          >
-            Sign In with Google
-          </Button>
-        )}
-      />
-      <Button
-        onClick={() => {}}
-        variant="outlined"
-        startIcon={<Apple />}
-        disabled
-      >
-        Coming soon...
-        {/* Sign In with Apple */}
-      </Button>
-      <Button
-        onClick={() => {}}
-        variant="outlined"
-        startIcon={<GitHub />}
-        disabled
-      >
-        Coming soon...
-        {/* Sign In with GitHub */}
-      </Button>
-      <Button
-        onClick={() => {}}
-        variant="outlined"
-        startIcon={<Twitter />}
-        disabled
-      >
-        Coming soon...
-        {/* Sign In with Twitter */}
-      </Button>
-      <FacebookLogin
-        appId={process.env.REACT_APP_FACEBOOK_APP_ID}
-        callback={responseFacebook}
-        component={(renderProps: any) => (
-          <Button
-            onClick={renderProps.onClick}
-            variant="outlined"
-            startIcon={<Facebook />}
-            disabled
-          >
-            Coming soon...
-            {/* Sign In with Facebook */}
-          </Button>
-        )}
-      />
+      <WaitFor isLoading={isLoading}>
+        <GoogleLogin
+          clientId={process.env.REACT_APP_GOOGLE_APP_ID as string}
+          onSuccess={responseSuccessGoogle}
+          onFailure={responseErrorGoogle}
+          cookiePolicy="single_host_origin"
+          render={(renderProps) => (
+            <Button
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+              variant="outlined"
+              startIcon={<GoogleIcon />}
+            >
+              Sign In with Google
+            </Button>
+          )}
+        />
+        <Button
+          onClick={() => {}}
+          variant="outlined"
+          startIcon={<Apple />}
+          disabled
+        >
+          Coming soon...
+          {/* Sign In with Apple */}
+        </Button>
+        <Button
+          onClick={() => {}}
+          variant="outlined"
+          startIcon={<GitHub />}
+          disabled
+        >
+          Coming soon...
+          {/* Sign In with GitHub */}
+        </Button>
+        <Button
+          onClick={() => {}}
+          variant="outlined"
+          startIcon={<Twitter />}
+          disabled
+        >
+          Coming soon...
+          {/* Sign In with Twitter */}
+        </Button>
+        <FacebookLogin
+          appId={process.env.REACT_APP_FACEBOOK_APP_ID}
+          callback={responseFacebook}
+          component={(renderProps: any) => (
+            <Button
+              onClick={renderProps.onClick}
+              variant="outlined"
+              startIcon={<Facebook />}
+              disabled
+            >
+              Coming soon...
+              {/* Sign In with Facebook */}
+            </Button>
+          )}
+        />
+      </WaitFor>
     </Container>
   )
 }
