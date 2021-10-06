ca143a19fb8b4a17e78545898a4c631b75be8edf
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-10-05 19:46:24 -0500

    Adding some basic eslint rules

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -4,9 +4,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'plugin:node/recommended', 'plugin:security/recommended', 'prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -15,9 +13,22 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'object-curly-newline': ['error', { ObjectPattern: { multiline: true } }],
+    semi: ['error', 'never'],
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        printWidth: 100,
+        semi: false,
+      },
+    ],
   },
-};
+}
