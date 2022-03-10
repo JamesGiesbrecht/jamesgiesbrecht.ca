cb14c49949b0fce6fda02adfbfd7f9f36733627a
James Giesbrecht <giesbrechtjames@gmail.com>
2022-03-09 20:12:04 -0600

    Removing unnecessary proxy rewrite

--- next-client/next.config.js
+++ next-client/next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => [
-    {
-      source: '/api/:slug*',
-      destination: `http://localhost:3001/api/:slug*`,
-    },
-  ],
   typescript: {
     // FIXME
     // !! WARN !!
