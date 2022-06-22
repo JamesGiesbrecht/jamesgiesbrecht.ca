fc0d9ced39651c2eac8a69144bf608c53aee39d2
James Giesbrecht <giesbrechtjames@gmail.com>
2022-06-21 19:24:10 -0500

    Debugging logs

--- src/pages/plex.tsx
+++ src/pages/plex.tsx
@@ -35,6 +35,7 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 export async function getServerSideProps() {
   let plexStats = null
+  console.log(`http://localhost:${serverRuntimeConfig.SERVER_PORT}/api/plex/sessions`)
   try {
     const result = await axios.get<GetPlexStatusResponse>(
       `http://localhost:${serverRuntimeConfig.SERVER_PORT}/api/plex/sessions`,
